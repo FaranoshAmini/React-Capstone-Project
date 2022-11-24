835c59efa0bb28db7c6cfaa9cfa911a479a7c8d8
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-24 13:19:54 +0430

    Created store using configureStore

---
+++ src/redux/configureStore.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import coinsReducer from './coins/coinsReducer';
+
+const store = configureStore({
+  reducer: {
+    coins: coinsReducer,
+
+  },
+});
+
+export default store;
