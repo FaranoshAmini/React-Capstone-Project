f0deffb1f67dea63b0b0a953bfc0869c8f653176
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-25 01:49:47 +0430

    Imported Dashboard, CoinCard, NavBar and Details to component.test.js

--- src/tests/component.test.js
+++ src/tests/component.test.js
@@ -3,3 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from '../redux/configureStore';
+import Dashboard from '../Pages/Dashboard';
+import CoinCard from '../components/CoinCard';
+import NavBar from '../components/NavBar';
+import Details from '../components/Details';
