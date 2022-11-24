5fb37e41c11ce25ce8d6f213385784722331f317
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-25 01:55:32 +0430

    Tests for API endpoint

---
+++ src/tests/api.test.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import fetchCoins, { url } from '../redux/Api/endpoint';
+
+jest.mock('axios');
+
+describe('Fetch Coin Data', () => {
+  it('returns Coin Ranking', async () => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          name: 'Ethereum',
+          image: '',
+          rank: 2,
+          currentPrice: '234',
+        },
+      ],
+    });
+
+    const result = await fetchCoins();
+
+    expect(axios.get).toHaveBeenCalledWith(url);
+    expect(result[0].rank).toEqual(2);
+  });
+});
