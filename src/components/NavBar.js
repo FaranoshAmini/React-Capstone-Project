d8359ebe25acbb64fd43740d205568bf77d7a574
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-24 16:44:51 +0430

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { BiArrowBack } from 'react-icons/bi';
 
 const NavBar = () => (
   <nav>
-    NavBar
+    <div className="nav-btn">
+      <Link to="/" className="btn-link">
+        <BiArrowBack />
+      </Link>
+    </div>
+    <div className="logo">
+      Crypto
+      <span>Coins</span>
+    </div>
   </nav>
 );
 
