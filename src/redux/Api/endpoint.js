7adc342fd0174783724ce44fffd8ab110f747e2b
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-24 12:52:46 +0430

    Create endpoint.js

---
+++ src/redux/Api/endpoint.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false&price_change_percentage=1h';
+
+const fetchCoins = () => {
+    
+}
