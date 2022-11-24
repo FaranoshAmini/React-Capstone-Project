dcbd5fc8be982c3ec2d89202b015d11af0f3ffc2
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-25 01:50:58 +0430

    Tests for UI components

--- src/tests/component.test.js
+++ src/tests/component.test.js
@@ -7,3 +7,55 @@ import Dashboard from '../Pages/Dashboard';
 import CoinCard from '../components/CoinCard';
 import NavBar from '../components/NavBar';
 import Details from '../components/Details';
+
+describe('UI Tests', () => {
+  it('should render Dashboard', () => {
+    const tree = render(
+      <Provider store={store}>
+        <Dashboard />
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('contains an image and renders CoinCard component', () => {
+    const coins = {
+      name: 'Ethereum',
+      image: '',
+      rank: 2,
+      currentPrice: '234',
+    };
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <CoinCard coin={coins} />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const image = screen.getByRole('img');
+    expect(image).toBeTruthy();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render NavBar', () => {
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <NavBar />
+        </BrowserRouter>
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render Details Component', () => {
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Details />
+        </BrowserRouter>
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
