3336a2a6d91eedd99e7675bc689c0d5c914d5f60
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-25 01:47:35 +0430

    Imported store to the component.test.js

--- src/tests/component.test.js
+++ src/tests/component.test.js
@@ -2,3 +2,4 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from '../redux/configureStore';
