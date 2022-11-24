cf7571ef9a8f67192de0b90bf876de5f8e768def
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-24 16:41:58 +0430

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <main>
         <Outlet />
       </main>
