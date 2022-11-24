8e93b3110d77621f5f68d538f609add428369605
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-24 13:00:57 +0430

    Fetch data from API

--- src/redux/Api/endpoint.js
+++ src/redux/Api/endpoint.js
@@ -2,6 +2,17 @@ import axios from 'axios';
 
 const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false&price_change_percentage=1h';
 
-const fetchCoins = () => {
-    
-}
+const fetchCoins = async () => {
+  const res = await axios.get(url);
+  const { data } = res;
+  const coins = data.map((coin) => ({
+    id: coin.id,
+    name: coin.name,
+    image: coin.image,
+    rank: coin.market_cap_rank,
+    price: coin.current_price,
+  }));
+  return coins;
+};
+
+export default fetchCoins;
